5ce3f8aee145441ce20f706209954eba6c8b8746
Chidiebere Chukwuma <chukschiboy@gmail.com>
2023-06-10 14:58:34 +0100

    update of the shipment screen

---
+++ lib/screens/shipment/pending.dart
@@ -0,0 +1,169 @@
+import 'package:flutter/material.dart';
+
+import '../../constants/color.dart';
+
+class Pending extends StatefulWidget {
+  const Pending({super.key});
+
+  @override
+  State<Pending> createState() => _PendingState();
+}
+
+class _PendingState extends State<Pending> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // margin: const EdgeInsets.all(40.0),
+      height: 120.0,
+      width: double.infinity / 2,
+      // color: Colors.white,
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.all(
+          Radius.circular(10),
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black12,
+            blurRadius: 15.0, // soften the shadow
+            spreadRadius: 3.0, //extend the shadow
+            offset: Offset(
+              1.0, // Move to right 5  horizontally
+              1.0, // Move to bottom 5 Vertically
+            ),
+          ),
+        ],
+      ),
+      // Shipment history content
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Content One
+              Align(
+                alignment: Alignment.topLeft,
+                child: Container(
+                  width: 75.0,
+                  height: 20.0,
+                  decoration: const BoxDecoration(
+                    color: Color(0xFFF8F1F1),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10),
+                    ),
+                  ),
+                  child: const Row(
+                    // crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.all(2.0),
+                        child: Icon(
+                          Icons.history,
+                          size: 10.0,
+                          color: Colors.orange,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(2.0),
+                        child: Text(
+                          "pending",
+                          style: TextStyle(
+                            fontSize: 8.0,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.orange,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              // Content Two
+              Padding(
+                padding: const EdgeInsets.only(top: 5.0, bottom: 5.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Arriving today!",
+                          style: TextStyle(
+                            fontSize: 12.0,
+                            fontWeight: FontWeight.w600,
+                            color: Colors.black,
+                          ),
+                        ),
+                        Text(
+                          "Your delivery #NEJ20089934122231 \n from Atlanta, is arriving today!",
+                          style: TextStyle(
+                            fontSize: 7.0,
+                            fontWeight: FontWeight.w300,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      width: 50.0,
+                      height: 50.0,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        gradient: LinearGradient(
+                          colors: [
+                            primaryColor,
+                            secondaryColor,
+                          ],
+                          begin: Alignment.topRight,
+                          end: Alignment.bottomLeft,
+                        ),
+                      ),
+                      child: const Icon(
+                        Icons.cases_rounded,
+                        color: Colors.white,
+                        size: 25.0,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              // Content Three
+              const Row(
+                children: [
+                  Text(
+                    "\$650 USD",
+                    style: TextStyle(
+                      fontSize: 8.0,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF320B75),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(3.0),
+                    child: Icon(
+                      Icons.circle,
+                      size: 5.0,
+                      color: Colors.grey,
+                    ),
+                  ),
+                  Text(
+                    "Sep 20, 2023",
+                    style: TextStyle(
+                      fontSize: 8.0,
+                      fontWeight: FontWeight.w300,
+                      color: Colors.black87,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/shipment/shipment_screen.dart
+++ lib/screens/shipment/shipment_screen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:move_mate/screens/shipment/in_progress.dart';
+import 'package:move_mate/screens/shipment/loading.dart';
+import 'package:move_mate/screens/shipment/pending.dart';
+
+import '../../constants/color.dart';
 
 class ShipmentScreen extends StatefulWidget {
   const ShipmentScreen({super.key});
@@ -10,6 +15,274 @@ class ShipmentScreen extends StatefulWidget {
 class _ShipmentScreenState extends State<ShipmentScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 100.0,
+        flexibleSpace: Container(
+          color: primaryColor,
+          child: const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Icon(
+                      Icons.arrow_back_ios,
+                      color: Colors.white,
+                      size: 13,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 12.0),
+                      child: Center(
+                        child: Text(
+                          "Shipment history",
+                          style: TextStyle(fontSize: 15, color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Row(
+                          children: [
+                            Text(
+                              "All",
+                              style: TextStyle(
+                                color: Colors.white24,
+                                fontWeight: FontWeight.w100,
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(left: 3.0),
+                              child: Text(
+                                "12",
+                                style: TextStyle(
+                                  color: Colors.white54,
+                                  fontWeight: FontWeight.w100,
+                                  fontSize: 12,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Row(
+                          children: [
+                            Text(
+                              "Completed",
+                              style: TextStyle(
+                                color: Colors.white24,
+                                fontWeight: FontWeight.w100,
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(left: 3.0),
+                              child: Text(
+                                "5",
+                                style: TextStyle(
+                                  color: Colors.white54,
+                                  fontWeight: FontWeight.w100,
+                                  fontSize: 12,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Row(
+                          children: [
+                            Text(
+                              "In Progress",
+                              style: TextStyle(
+                                color: Colors.white24,
+                                fontWeight: FontWeight.w100,
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(left: 3.0),
+                              child: Text(
+                                "3",
+                                style: TextStyle(
+                                  color: Colors.white54,
+                                  fontWeight: FontWeight.w100,
+                                  fontSize: 12,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Row(
+                          children: [
+                            Text(
+                              "Pending",
+                              style: TextStyle(
+                                color: Colors.white24,
+                                fontWeight: FontWeight.w100,
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(left: 3.0),
+                              child: Text(
+                                "3",
+                                style: TextStyle(
+                                  color: Colors.white54,
+                                  fontWeight: FontWeight.w100,
+                                  fontSize: 12,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Row(
+                          children: [
+                            Text(
+                              "Cancelled",
+                              style: TextStyle(
+                                color: Colors.white24,
+                                fontWeight: FontWeight.w100,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      body: ScrollConfiguration(
+        behavior: ScrollConfiguration.of(context).copyWith(scrollbars: false),
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Container(
+            padding: const EdgeInsets.all(8.0),
+            margin: const EdgeInsets.all(8.0),
+            child: const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Align(
+                alignment: Alignment.topLeft,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Shipments",
+                      style: TextStyle(
+                        fontSize: 13.0,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.black,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    // First Container
+                    InProgress(),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    Pending(),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    Loading(),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    // First Container
+                    InProgress(),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    Pending(),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    Loading(),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    // First Container
+                    InProgress(),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    Pending(),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    Loading(),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    // First Container
+                    InProgress(),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    Pending(),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    Loading(),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    // First Container
+                    InProgress(),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    Pending(),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    Loading(),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
