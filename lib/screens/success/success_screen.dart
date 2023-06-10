1d4c883ec93c0b5372a081d2e72874eceba26eaa
Chidiebere Chukwuma <chukschiboy@gmail.com>
2023-06-10 12:57:43 +0100

    success screen created and developed

--- lib/screens/success/success_screen.dart
+++ lib/screens/success/success_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:move_mate/constants/color.dart';
+import 'package:move_mate/screens/home/home_screen.dart';
 
 class SuccessScreen extends StatefulWidget {
   const SuccessScreen({super.key});
@@ -10,6 +12,108 @@ class SuccessScreen extends StatefulWidget {
 class _SuccessScreenState extends State<SuccessScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  height: 50.0,
+                ),
+                SizedBox(
+                  width: 250.0,
+                  height: 70.0,
+                  child: Image.asset("assets/move_logo.png"),
+                ),
+                const SizedBox(
+                  height: 20.0,
+                ),
+                Container(
+                  width: 150.0,
+                  height: 150.0,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    gradient: LinearGradient(
+                      colors: [
+                        primaryColor,
+                        secondaryColor,
+                      ],
+                      begin: Alignment.topRight,
+                      end: Alignment.bottomLeft,
+                    ),
+                  ),
+                  child: const Icon(
+                    Icons.cases_rounded,
+                    color: Colors.white,
+                    size: 80.0,
+                  ),
+                ),
+                const SizedBox(
+                  height: 20.0,
+                ),
+                const Text(
+                  "Total Estimated Amount",
+                  style: TextStyle(
+                    fontSize: 19.0,
+                    fontWeight: FontWeight.w300,
+                    color: Colors.black,
+                  ),
+                ),
+                const SizedBox(
+                  height: 10.0,
+                ),
+                const Text(
+                  "\$ 1460 USD",
+                  style: TextStyle(
+                    fontSize: 22.0,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.green,
+                  ),
+                ),
+                const SizedBox(
+                  height: 10.0,
+                ),
+                const Text(
+                  "This amount is estimated this will vary \n if you change your location or weight",
+                  style: TextStyle(
+                    fontSize: 12.0,
+                    fontWeight: FontWeight.w300,
+                    color: Colors.grey,
+                  ),
+                ),
+                const SizedBox(
+                  height: 30.0,
+                ),
+                SizedBox(
+                  width: double.infinity / 2,
+                  height: 50.0,
+                  child: ElevatedButton(
+                    onPressed: () => _navigateToNextScreen(context),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: secondaryColor,
+                      foregroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(32.0),
+                      ),
+                    ),
+                    child: const Text("Back to home"),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _navigateToNextScreen(BuildContext context) {
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (context) => const HomeScreen()));
   }
 }
